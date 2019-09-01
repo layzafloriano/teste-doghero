925b5a2814abf92d4055f378c03824601b153982
Layza Floriano <layzafloriano@gmail.com>
2019-09-01 12:03:54 -0300

    Add navbar

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
+import { RouterModule } from '@angular/router';
 
 
 
@@ -8,6 +9,7 @@ import { HeaderComponent } from './header/header.component';
   declarations: [HeaderComponent],
   imports: [
     CommonModule,
+    RouterModule
   ],
   exports: [
     HeaderComponent
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Menu } from 'src/app/interfaces/menu.interface';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +7,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  menu: Menu[];
 
   constructor() { }
 
   ngOnInit() {
+    this.setMenuByProfile();
+  }
+
+  setMenuByProfile() {
+    const userData = { role: 'dogwalker' };
+    if (userData.role === 'dogwalker') {
+      this.setMenuDogwalker();
+    } else {
+      this.setMenuClient();
+    }
+  }
+
+  setMenuDogwalker() {
+    this.menu = [
+      {
+        text: 'Home',
+        path: '/',
+      },
+      {
+        text: 'Clients',
+        path: '/clients',
+      },
+      {
+        text: 'Dogs',
+        path: '/dogs',
+      }
+    ];
+  }
+
+  setMenuClient() {
+    this.menu = [
+      {
+        text: 'Home',
+        path: '/',
+      },
+      {
+        text: 'Dog Walkers',
+        path: '/dog-walkers',
+      },
+    ];
   }
 
 }
