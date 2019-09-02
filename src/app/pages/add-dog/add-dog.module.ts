f59a1da7165a24a5efe8823eeef3f78e8a8d03b8
Layza Floriano <layzafloriano@gmail.com>
2019-09-02 11:15:37 -0300

    add header to add dog

--- src/app/pages/add-dog/add-dog.module.ts
+++ src/app/pages/add-dog/add-dog.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AddDogComponent } from './add-dog.component';
+import { ComponentsModule } from 'src/app/components/components.module';
 
 
 
 @NgModule({
   declarations: [AddDogComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    ComponentsModule
   ]
 })
 export class AddDogModule { }
